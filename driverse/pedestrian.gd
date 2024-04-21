1a0ff336c43e1dd88ee10a3bf2fae7ba1490e07e
adahe8 <ada.he0908@gmail.com>
2024-04-21 07:53:01 -0700

    tweaked pedestrian animation

--- driverse/pedestrian.gd
+++ driverse/pedestrian.gd
@@ -1,7 +1,8 @@
-extends CharacterBody2D
-
+extends CharacterBody2D 
 
 const SPEED = 300.0
+const ACCELERATION = 200.0
+const FRICTION = 500
 const JUMP_VELOCITY = -400.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
@@ -13,16 +14,12 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	var input_vector = Vector2.ZERO
+	input_vector = input_vector.normalized()
+	
+	if input_vector!=Vector2.ZERO:
+		velocity = velocity.move_toward(input_vector*SPEED, ACCELERATION*delta)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION*delta)
 
 	move_and_slide()
