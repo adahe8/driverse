26c72b0c29efeeaf2ecd15f71c023abddae98e8c
CarmenHe <43501759+CarmenHe@users.noreply.github.com>
2024-04-21 05:16:11 -0700

    edits to pedestrians script

---
+++ driverse/pedestrian-spawner.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+@onready var timer = $Timer
+
+var Pedestrian = preload("res://pedestrian.tscn")
+
+func _ready():
+	randomize()
+
+func _on_Timer_timeout():
+	spawn_pedestrian() # Replace with function body.
+
+func spawn_pedestrian():
+	var pedestrian = Pedestrian.instance()
+	add_child(pedestrian)
+	# Get a random number between 100 to
+	pedestrian.position.y = randi()%400+100
+	
+
+func start():
+	timer.start()
+
+func stop():
+	timer.stop()
