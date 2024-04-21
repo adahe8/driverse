fbb05574b987852d44b8589d956fc833371058ae
adahe8 <ada.he0908@gmail.com>
2024-04-21 01:19:20 -0700

    borders on roadsides!

--- driverse/user_car.gd
+++ driverse/user_car.gd
@@ -2,16 +2,12 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -22,3 +18,7 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+func _on_area_2d_area_entered(area):
+	if area.is_in_group("leftside") or area.is_in_group("rightside"):
+		print("too close to the edge of the road!")
